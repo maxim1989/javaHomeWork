4239627127db242912955655dcff5027f781bb5a
maksimfedorov <fedorovmax25011989@gmail.com>
2024-06-12 14:42:40 +0300

    Lesson 134100, fix task 10

--- src/Main.java
+++ src/Main.java
@@ -74,7 +74,7 @@ public class Main {
 
     public static void task10() {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(2 * i);
+            System.out.printf("2 * %d = %d\n", i, 2 * i);
         }
     }
 }
