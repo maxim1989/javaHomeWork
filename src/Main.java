7fefe44e23d38a10255423759b6563c89a42de6a
maksimfedorov <fedorovmax25011989@gmail.com>
2024-06-17 23:00:50 +0300

    Lesson 134102, tasks from 1 to 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,93 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static void task1() {
+        int[] intArray = new int[]{
+                1, 2, 3
+        };
+        float[] floatArray = {1.57f, 7.654f, 9.986f};
+        int[] randomArray = new int[]{2, 0};
+    }
+
+    public static void task2() {
+        int[] intArray = new int[]{
+                1, 2, 3
+        };
+        for (int i = 0; i < intArray.length; i++) {
+            if (i == intArray.length - 1) {
+                System.out.print(intArray[i] + "\n");
+                break;
+            }
+            System.out.print(intArray[i] + ", ");
+        }
+
+        float[] floatArray = {1.57f, 7.654f, 9.986f};
+        for (int j = 0; j < floatArray.length; j++) {
+            if (j == floatArray.length - 1) {
+                System.out.print(floatArray[j] + "\n");
+                break;
+            }
+            System.out.print(floatArray[j] + ", ");
+        }
+
+        int[] randomArray = new int[]{2, 0};
+        for (int k = 0; k < randomArray.length; k++) {
+            if (k == randomArray.length - 1) {
+                System.out.print(randomArray[k] + "\n");
+                break;
+            }
+            System.out.print(randomArray[k] + ", ");
+        }
+    }
+
+    public static void task3() {
+        int[] intArray = new int[]{
+                1, 2, 3
+        };
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(intArray[i] + "\n");
+                break;
+            }
+            System.out.print(intArray[i] + ", ");
+        }
+
+        float[] floatArray = {1.57f, 7.654f, 9.986f};
+        for (int j = floatArray.length - 1; j >= 0 ; j--) {
+            if (j == 0) {
+                System.out.print(floatArray[j] + "\n");
+                break;
+            }
+            System.out.print(floatArray[j] + ", ");
+        }
+
+        int[] randomArray = new int[]{2, 0};
+        for (int k = randomArray.length - 1; k >= 0 ; k--) {
+            if (k == 0) {
+                System.out.print(randomArray[k] + "\n");
+                break;
+            }
+            System.out.print(randomArray[k] + ", ");
+        }
+    }
+
+    public static void task4() {
+        int[] intArray = new int[]{
+                1, 2, 3
+        };
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] % 2 != 0) {
+                intArray[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(intArray));
     }
 }
