a3d83b97c192d5a1bd17cc2a5fa3979fbd49ea43
maksimfedorov <fedorovmax25011989@gmail.com>
2024-07-09 20:52:03 +0300

    Course final work 19488, basic difficulty fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-    private static final Employee[] employees = new Employee[10];
-
     public static void main(String[] args) {
+        final Employee[] employees = new Employee[10];
         for (int i = 1, j = 1; i <= 10; i++) {
             float baseSalary = 1000.5f;
             Employee employee = new Employee("Name" + i, "Surname" + i, "SecondName" + i, j, baseSalary * j);
@@ -11,62 +10,85 @@ public class Main {
             }
             employees[i - 1] = employee;
         }
-        printAllEmployees();
-        System.out.println(calculateMonthSalaryForAllEmployees());
-        System.out.println(findEmployeeWithMinSalary());
-        System.out.println(findEmployeeWithMaxSalary());
-        System.out.println(calculateMediumSalary());
-        printAllEmployeesFio();
+        printAllEmployees(employees);
+        System.out.println(calculateMonthSalaryForAllEmployees(employees));
+        System.out.println(findEmployeeWithMinSalary(employees));
+        System.out.println(findEmployeeWithMaxSalary(employees));
+        System.out.println(calculateMediumSalary(employees));
+        printAllEmployeesFio(employees);
     }
 
-    public static void printAllEmployees() {
+    public static void printAllEmployees(Employee[] employees) {
         for (Employee employee : employees) {
-            System.out.println(employee);
+            if (employee != null) {
+                System.out.println(employee);
+            }
         }
     }
 
-    public static float calculateMonthSalaryForAllEmployees() {
+    public static float calculateMonthSalaryForAllEmployees(Employee[] employees) {
         float monthSalary = 0f;
         for (Employee employee : employees) {
-            monthSalary += employee.getSalary();
+            if (employee != null) {
+                monthSalary += employee.getSalary();
+            }
         }
 
         return monthSalary;
     }
 
-    public static Employee findEmployeeWithMinSalary() {
-        Employee employeeWithMinSalary = employees[0];
-        float minSalary = employeeWithMinSalary.getSalary();
-        for (int i = 1; i < employees.length; i++) {
-            float salary = employees[i].getSalary();
-            if (minSalary > salary) {
-                minSalary = salary;
-                employeeWithMinSalary = employees[i];
+    public static Employee findEmployeeWithMinSalary(Employee[] employees) {
+        Employee employeeWithMinSalary = null;
+        float minSalary = -1.0f;
+
+        for (Employee employee : employees) {
+            if (employee != null) {
+                float currentSalary = employee.getSalary();
+                if (minSalary < 0 || minSalary > 0 && minSalary > currentSalary) {
+                    minSalary = currentSalary;
+                    employeeWithMinSalary = employee;
+                }
             }
         }
+
         return employeeWithMinSalary;
     }
 
-    public static Employee findEmployeeWithMaxSalary() {
-        Employee employeeWithMaxSalary = employees[0];
-        float maxSalary = employeeWithMaxSalary.getSalary();
-        for (int i = 1; i < employees.length; i++) {
-            float salary = employees[i].getSalary();
-            if (maxSalary < salary) {
-                maxSalary = salary;
-                employeeWithMaxSalary = employees[i];
+    public static Employee findEmployeeWithMaxSalary(Employee[] employees) {
+        Employee employeeWithMaxSalary = null;
+        float maxSalary = -1.0f;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                float currentSalary = employee.getSalary();
+                if (maxSalary < 0 || maxSalary > 0 && maxSalary < currentSalary) {
+                    maxSalary = currentSalary;
+                    employeeWithMaxSalary = employee;
+                }
             }
         }
         return employeeWithMaxSalary;
     }
 
-    public static float calculateMediumSalary() {
-        return calculateMonthSalaryForAllEmployees() / employees.length;
+    public static float calculateMediumSalary(Employee[] employees) {
+        int employeesAmount = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                employeesAmount++;
+            }
+        }
+
+        if (employeesAmount == 0) {
+            return employeesAmount;
+        }
+
+        return calculateMonthSalaryForAllEmployees(employees) / employeesAmount;
     }
 
-    public static void printAllEmployeesFio() {
+    public static void printAllEmployeesFio(Employee[] employees) {
         for (Employee employee : employees) {
-            System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getSecondName());
+            if (employee != null) {
+                System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getSecondName());
+            }
         }
     }
 }
