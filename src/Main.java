e8681a15e5488ff8d91de26514219e25c4da8116
maksimfedorov <fedorovmax25011989@gmail.com>
2025-01-19 18:19:49 +0300

    Lesson 134141, палиндром

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome("a"));
+        System.out.println(isPalindrome("potop"));
+        System.out.println(isPalindrome("ab"));
+        System.out.println(isPalindrome("aba"));
+        System.out.println(isPalindrome("abac"));
+    }
+
+    public static boolean isPalindrome(String word) {
+        int cursorLeft = 0;
+        int cursorRight = word.length() - 1;
+
+        while (cursorLeft <= cursorRight) {
+            if (word.charAt(cursorLeft) != word.charAt(cursorRight)) {
+                return false;
+            }
+
+            cursorLeft++;
+            cursorRight--;
+        }
+
+        return true;
     }
 }
